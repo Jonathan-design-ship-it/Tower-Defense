589aeaf19bee6d33419b3316f5263caabd347368
tf12345678 <150176267+tf12345678@users.noreply.github.com>
2025-01-15 14:45:49 -0500

    added grid

--- Tower Defense/GameWorld.java
+++ Tower Defense/GameWorld.java
@@ -1,6 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Write a description of class GameWorld here.
@@ -37,6 +39,8 @@ public class GameWorld extends World
         map1Path = new ArrayList<Coordinate>();
         map2Path = new ArrayList<Coordinate>();
         map3Path = new ArrayList<Coordinate>();
+        
+        grid();
 
         map = 1;
         if (map == 1){
@@ -155,5 +159,40 @@ public class GameWorld extends World
             return map2Path.get(num);
         // if map == 3
         return map3Path.get(num);
+    }
+    
+    public void grid()
+    {
+        Coordinate[][] positions = new Coordinate[9][9];
+        for(int i = 0; i < 9; i++)
+        {
+            for(int n = 0; n < 9; n++)
+            {
+                positions[n][i] = new Coordinate(80 + (92 * i), 51 + (92 * n));
+            }
+        }
+        
+        Integer[][] remove = {{4, 6, 7, 8},
+                              {1, 2, 3, 4, 6, 8},
+                              {1, 6, 8},
+                              {1, 2, 3, 4, 5, 6, 8},
+                              {0, 8},
+                              {0, 3, 4, 5, 8},
+                              {0, 3, 5, 8},
+                              {0, 1, 2, 3, 5, 6, 7, 8},
+                              {0, 1, 2, 3, 4, 5, 6, 7, 8}
+                             };
+        
+        for (int i = 0; i < positions.length; i++) {
+            for (int j = 0; j < positions[i].length; j++) {
+                if(!Arrays.asList(remove[j]).contains(i))
+                {
+                    // if int == max value, inaccesible
+                    positions[i][j].setX(Integer.MAX_VALUE);
+                    positions[i][j].setY(Integer.MAX_VALUE);
+                }
+            }
+            System.out.println(); // Move to the next line after each row
+        }
     }
 }
